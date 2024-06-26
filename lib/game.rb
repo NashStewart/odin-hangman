9abb61709f0d1d402c16464af0fd496a8265f4fc
Nash Stewart <nashkstewart@gmail.com>
2024-06-25 23:11:39 -0600

    Add guard clauses to guess methods
    Add guard classes to guess methods to ensure that there are guesses
    left, the guess has not already been made, and that the guess is of the
    proper length. Add a getter for secret_word that hides the un-guessed
    letters.

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@
 
 # Object to keep track of game state.
 class Game
-  attr_reader :correct_guesses, :guesses_left, :incorrect_guesses, :secret_word
+  attr_reader :correct_guesses, :guesses_left, :incorrect_guesses
 
   def initialize(secret_word)
     @secret_word = secret_word
@@ -12,16 +12,40 @@ class Game
   end
 
   def guess_letter(letter)
+    return unless guesses_left > 0
+    return unless letter.length == 1
+    return if already_guessed? letter
+
+    @guesses_left -= 1
     @secret_word.include?(letter) ? @correct_guesses << letter : @incorrect_guesses << letter
   end
 
-  def word_is?(word)
-    @secret_word == word
+  def guess_word(word)
+    return unless guesses_left > 0
+    return unless word
+    return if already_guessed? word
+
+    @guesses_left -= 1
+    @secret_word == word ? @correct_guesses << word : @incorrect_guesses << word 
   end
 
+  def secret_word
+    return @secret_word.split('').join(' ') if @correct_guesses.include? @secret_word
+    @secret_word.split('').map { |char| @correct_guesses.include?(char) ? "#{char} " : "_ " }.join
+  end
+
+  def debug_secret_word
+    @secret_word
+  end
+
+  private
+
+  def already_guessed?(guess)
+    correct_guesses.include?(guess) || incorrect_guesses.include?(guess)
+  end
   # x Make a letter guess
   # x Make a word guess
-  # - Get player version of secret_word
+  # x Get player version of secret_word
   # - Serialize
   # - Deserialize
 end
--- main.rb
+++ main.rb
@@ -6,13 +6,38 @@ require_relative 'lib/words'
 secret_word = Words.random_word
 game = Game.new secret_word
 
-pp game
-game.guess_letter('l')
-pp game
+p game
+game.guess_letter('a')
+p game
+game.guess_letter('e')
+p game
+game.guess_letter('i')
+p game
+game.guess_letter('o')
+p game
+game.guess_letter('o') # Redundant guess
+p game
+# game.guess_letter('u')
+# p game
+# game.guess_letter('y') # Sixth guess
+# p game
+# game.guess_letter('x') # Seventh guess
+#p game
+
+puts
 
 incorrect_word = 'bubble'
-correct_word = game.secret_word.clone
 p incorrect_word
+game.guess_word incorrect_word
+p game
+game.guess_word incorrect_word # Redundant guess
+p game
+puts game.secret_word
+
+puts
+
+correct_word = game.debug_secret_word.clone
 p correct_word
-p game.word_is? incorrect_word
-p game.word_is? correct_word
+game.guess_word correct_word
+p game
+puts game.secret_word
